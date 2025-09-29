eba382916d0f95b763932bedbf7b63b82a760a81
Unju6g <shahagunjan33@gmail.com>
2025-09-30 00:21:41 +0530

    quetion_1.java

---
+++ quetion_1.java
@@ -0,0 +1,14 @@
+import java.time.LocalDate;
+public class quetion_1 {
+    public static void main(String[] args) {
+        String Name ="Gunjan shaha";
+        String CollegeName =" Walchand institute of technology";
+        LocalDate Date =LocalDate.now();
+
+
+        System.out.println("name         :"+Name);
+        System.out.println("collegename  :" +CollegeName);
+        System.out.println("date         :"+Date);
+    }
+}
+
