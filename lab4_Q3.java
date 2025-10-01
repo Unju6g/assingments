45c03dfc5ae7fdb76d9146dab3d07c048ce52b97
Unju6g <shahagunjan33@gmail.com>
2025-10-02 00:21:58 +0530

    lab4_Q3.java

---
+++ lab4_Q3.java
@@ -0,0 +1,30 @@
+// Custom checked exception
+class MinorAgeException extends Exception {
+    public MinorAgeException(String message) {
+        super(message);
+    }
+}
+
+class VotingMachine {
+    
+    public void castVote(int age) throws MinorAgeException {
+        if (age < 18) {
+          
+            throw new MinorAgeException("Voter is not eligible to vote.");
+        } else {
+            System.out.println("Vote cast successfully.");
+        }
+    }
+
+    public static void main(String[] args) {
+        VotingMachine vm = new VotingMachine();
+
+        try {
+            
+            vm.castVote(16);
+        } catch (MinorAgeException e) {
+            
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
